ed1fe3b61ee702667c5e3d85a9d8efc086b61cb3
loganbwu <loganbwu@live.com>
2017-07-20 11:23:56 +1200

    Add daily trend analysis

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import datetime as dt
 import matplotlib.dates as mdates
 myFmt = mdates.DateFormatter('%b \'%y')
+plt.style.use('default')
 
-filename = 'spot_prices_head.csv'
-WINDOW = 48*30      # trading periods. 48=1 day
+filename = 'spot_prices.csv'
+WINDOW = 30         # days
+TREND_FREQ = 1      # days
+YEAR = 365          # days
 
 
 def average_price(df, datemin, datemax, weights=1):
@@ -28,22 +32,33 @@ def average_price(df, datemin, datemax, weights=1):
 # import data
 df = pd.read_csv(filename, dtype={'GIP_GXP': str})
 df['TRADING_DATE'] = pd.to_datetime(df['TRADING_DATE'], format='%d/%m/%y')
-df = df.set_index(df['TRADING_DATE'], drop=True)
 df['TRADING_DATE'] = df['TRADING_DATE'] + dt.timedelta(minutes=30) * df['TRADING_PERIOD']
+df = df.set_index(df['TRADING_DATE'], drop=True)
+
 
 # create time displacement since first date
 start_date = df['TRADING_DATE'][0]
 df['TIME_DELTA'] = df['TRADING_DATE'] - start_date
 df['PERIOD_DELTA'] = df['TIME_DELTA'].apply(lambda x: int(x.total_seconds()/1800))
 df = df.drop(['GIP_GXP', 'TRADING_PERIOD', 'TRADING_DATE'], axis=1)
+df.to_csv('filtered_data.csv')
+
+
+# perform regression
+df['YEAR'] = df.index.to_period('A')
+years = df['YEAR'].unique()
+resarr = []
+for year in years:
+    resarr.append(sm.tsa.seasonal_decompose(df[df['YEAR']==year]['PRICE'],
+                                            freq=48*TREND_FREQ))
+
 
 # calculate statistics
-df['ROLLING_MEAN'] = df['PRICE'].rolling(WINDOW, center=True, win_type='blackman').mean()
-#df['ROLLING_STD'] = df['PRICE'].rolling(WINDOW, center=True).std()
-
-print(df.head())
+df['ROLLING_MEAN'] = df['PRICE'].rolling(WINDOW*48, center=True, win_type='blackman').mean()
+#df['ROLLING_STD'] = df['PRICE'].rolling(WINDOW*48, center=True).std()
 mean = df['PRICE'].mean()
 std = df['PRICE'].std()
+
 
 # create plots
 fig, ax = plt.subplots()
@@ -51,6 +66,18 @@ ax.plot(df['PRICE'], '.', alpha=0.1)
 ax.plot(df['ROLLING_MEAN'])
 ax.set_ylim([max(mean-2*std, 0), mean+2.45*std])
 ax.xaxis.set_major_formatter(myFmt)
+ax.set_title('Smoothed Data (%d-day window)' % WINDOW)
+ax.legend(['Spot price', '%d-day trend' % TREND_FREQ, 'Rolling mean'])
+plt.show()
+
+
+fig, ax = plt.subplots()
+for res in resarr:
+    ax.plot(res.seasonal.reset_index(drop=True)[:TREND_FREQ*48], alpha=0.5)
+ax.set_title('Daily seaonal trend per year')
+ax.legend(years)
+
+
 
 print("Average price for January 2007: $%.2f" % average_price(df,'1/1/2007', '31/1/2007'))
 print("Average price for Q1 2007: $%.2f" % average_price(df,'1/1/2007', '31/3/2007'))
---
+++ regression.R
@@ -0,0 +1,1 @@
+data = read.csv('filtered_data.csv')
